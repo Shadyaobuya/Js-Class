23f427336a5a25187ba8014de3f1ad5f2579ea18
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-03-19 01:20:12 +0300

    class Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function name(){
+    var a = "Shadya";
+    var b="Obuya"
+  function identify(){
+    var c = a+" " +"and"+" "+b+ " are friends";
+          console.log(c)
+  }
+  identify();
+  }
+ name();
